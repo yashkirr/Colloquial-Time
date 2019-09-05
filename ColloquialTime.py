c9b38562245184e72caf1fb58a36c1fad607822e
yashkirr <yashkirramsamy@gmail.com>
2019-09-05 20:59:38 +0200

    Add files via upload

---
+++ ColloquialTime.py
@@ -0,0 +1,134 @@
+# Yashkir Ramsamy
+# 1 July 2019
+# Module for converting digital time format to a colloquial english time phrase
+
+
+import datetime
+
+
+numbers = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve"]
+timePhrases = ["five", "ten", "quarter", "twenty", "twenty-five", "half"]
+context = ["o' clock", "past", "to"]
+
+
+
+def getTime():
+    """
+    Summary
+       Gets current time and returns a string
+    ----------------------------------
+
+    :return:
+    -----------
+        list
+        returns H and M values in list of length 2
+
+
+    """
+    h = int('{:%H}'.format(datetime.datetime.now()))
+    m = int('{:%M}'.format(datetime.datetime.now()))
+    if h > 12:
+        h = h-12
+    return [h, m]
+
+
+
+def appropriateMinutes(m):
+    """
+    Summary
+        Rounds minutes used for use of determining colloquial phrase
+    ----------------------------------
+    :param m: int
+        Value of minutes
+    ----------------------------------
+    :return:
+    -----------
+        int
+        rounded minute value
+
+    """
+    if m > 30:
+        m = 60 - m
+    return 5 * round((m/5))
+
+
+
+def pastOrTo(m):
+    """
+    Summary
+        Determine "past" or "to" or "o' clock"
+    ----------------------------------
+    :param m: int
+        Value of minutes
+    ----------------------------------
+    :return:
+    -----------
+        str
+        returns the context of time
+
+    """
+    if m == 0:
+        return context[0]
+    elif m < 31:
+        return context[1]
+    else:
+        return context[2]
+
+
+
+def returnTime(h, m):
+    """
+    Summary
+        Returns H and M in a colloquial english phrase
+    ----------------------------------
+    :param h: int
+        Value of hour
+    :param m: int
+        Value of hour
+    ----------------------------------
+    :return:
+    -----------
+        str
+        returns colloquial english phrase
+
+    """
+    m = appropriateMinutes(m)
+    if (pastOrTo(m) == context[0]) or (m == 0): # check if time is currently o'clock or to be o' clock
+        if m == 0:
+            if h == 12: # if current time is 12:58+ , change to 1:00
+                return numbers[0]+" "+pastOrTo(m)
+            return numbers[h]+" "+pastOrTo(m) # if time is not 12:58+
+        else:
+            return numbers[h-1]+" "+pastOrTo(m)
+    else:
+        ptval = pastOrTo(m)
+        if m == 5:
+            index = 0
+        elif m == 10:
+            index = 1
+        elif m == 15:
+            index = 2
+        elif m == 20:
+            index = 3
+        elif m == 25:
+            index = 4
+        else:
+            index = 5
+        phase = timePhrases[index]
+
+        if ptval == "past":
+            return phase+" "+ptval+" "+numbers[h-1]  # [h-1] to select current hour
+        elif h == 12:
+            return phase+" "+ptval+" "+numbers[0]    # [0] to select "twelve"
+        else:
+            return phase+" "+ptval+" "+numbers[h]    # [h] to select the coming hour
+
+
+# example usage
+def main():
+    timeArr = getTime()
+    print(returnTime(timeArr[0], timeArr[1]))
+
+
+if __name__ == "__main__":
+    main()
